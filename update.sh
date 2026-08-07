2205e07db4e4c0a68a77d1f8630611eab9d8e975
Peter <stridsberg.peter@gmail.com>
2026-08-07 10:39:54 +0200

    Added docker to update

--- update.sh
+++ update.sh
@@ -30,6 +30,44 @@ git reset --hard HEAD
 ## Restart services to apply new files
 systemctl --user restart sony-ptz-demo.service
 
+##
+## Update docker image for companion
+##
+
+container_id=$(sudo docker ps -q --filter "name=Companion")
+if [ -n "$container_id" ]
+then
+    echo "INFO: Stopping and removing existing companion container"
+    sudo docker stop Companion
+    sudo docker rm Companion
+
+    image_id=$(sudo docker images -q ghcr.io/bitfocus/companion/companion:latest)
+    if [ -n "$image_id" ]
+    then
+        echo "INFO: Removing existing companion image"
+        sudo docker rmi ghcr.io/bitfocus/companion/companion:latest
+        echo "INFO: Finished removing existing companion image"
+    fi
+fi
+
+    image_id=$(sudo docker images -q ghcr.io/bitfocus/companion/companion:latest)
+    if [ ! -n "$image_id" ]
+    then
+        echo "INFO: No existing companion image found"
+        echo "INFO: Pulling latest companion image"
+        sudo docker pull ghcr.io/bitfocus/companion/companion:latest
+        sudo docker run -d --privileged -p 10000:8000 -v /companion:/companion --name "Companion" -v /dev/hidraw0:/dev/hidraw0 --restart always ghcr.io/bitfocus/companion/companion:latest
+        container_id=$(sudo docker ps -q --filter "name=Companion")
+        if [ -n "$container_id" ]
+        then
+            echo "INFO: Successfully started companion container"
+        else
+            echo "ERROR: Failed to start companion container"
+            exit 10
+        fi
+    fi
+fi
+
 echo ""
 echo "Update complete...."
 echo ""
